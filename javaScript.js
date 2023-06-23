00ebda33d8f1a6fd9021597496887b83786abe49
nabreesBM <137473584+nabreesBM@users.noreply.github.com>
2023-06-23 12:24:08 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,276 @@
+ 
+var img1 = document.getElementById('myImg1');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img1.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+        var img2 = document.getElementById('myImg2');
+       
+        var modal = document.getElementById('myModal'); 
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        var span = document.getElementsByClassName("close")[0];
+        
+        img2.onclick = function(){
+                  modal.style.display = "block";
+                  modalImg.src = this.src;
+                  captionText.innerHTML = this.alt;
+                  captionText.style.fontSize = "25px";
+                  captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+                }
+        
+        
+        span.onclick = function() { 
+                  modal.style.display = "none";
+                }
+        
+       
+// loop
+
+
+var img3 = document.getElementById('myImg3');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img3.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img4 = document.getElementById('myImg4');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img4.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img5 = document.getElementById('myImg5');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img5.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img6 = document.getElementById('myImg6');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img6.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img7 = document.getElementById('myImg7');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img7.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img8 = document.getElementById('myImg8');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img8.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img9 = document.getElementById('myImg9');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img9.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img10 = document.getElementById('myImg10');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img10.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img11 = document.getElementById('myImg11');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img11.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
+var img12 = document.getElementById('myImg12');
+       
+var modal = document.getElementById('myModal'); 
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+img12.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          captionText.style.fontSize = "25px";
+          captionText.style.textShadow = "2px 2px 3px rgb(216, 25, 11)";
+        }
+
+
+span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+
+// loop
+
